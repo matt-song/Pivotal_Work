6c15f49ccd3417c323e0966b5824c05c5f5dd04a
Matt Song <matt.song@live.cn>
2018-11-13 13:27:59 +0800

    Updated at 2018-11-13

--- scripts/sync_git.sh
+++ scripts/sync_git.sh
@@ -1,2 +1,2 @@
-rsync -av --exclude=".git*" ~/git_repository/ gpadmin@aio:~/git_repository/
+#rsync -av --exclude=".git*" ~/git_repository/ gpadmin@aio:~/git_repository/
 rsync -av --exclude=".git*" ~/git_repository/ gpadmin@aio2:~/git_repository/
