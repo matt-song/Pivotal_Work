9c23535741b2b75546856bc6eaacee4795e77264
Matt Song <matt.song@live.cn>
2018-12-10 14:48:12 +0800

    Updated at 2018-12-10

--- scripts/update_case.pl
+++ scripts/update_case.pl
@@ -2,15 +2,16 @@
 #################################################################
 # Author:      Matt Song                                        #
 # Create Date: 2018.12.03                                       #
-# Description: Generate template for case update                #
+# Description: Generate template for case management            #
 #################################################################
 use strict;
 use Data::Dumper;
 use Getopt::Std;
-my %opts; getopts('hDt:c:', \%opts);
+my %opts; getopts('hDt:c:l:', \%opts);
 my $DEBUG = $opts{'D'};   
 my $task = $opts{'t'};
 my $case_to_check = $opts{'c'};
+my $lang = $opts{'l'};
 
 my $work_folder = '/Users/xsong/work';
 
@@ -22,15 +23,11 @@ if ($task =~ /template/i)
 }
 elsif ($task =~ /find/i)
 {
-    if ($case_to_check)
-    {
-        &find_case_update($case_to_check);
-    }
-    else
-    {
-        print "No case number was specified, exit!";
-        &print_help;
-    }
+    &find_case_update($case_to_check);
+}
+elsif ($task =~ /ir/i)
+{
+    generate_ir($lang);
 }
 else
 {
@@ -38,7 +35,7 @@ else
 }
 
 
-
+##### functions #####
 sub print_help
 {
     print "
@@ -47,12 +44,66 @@ sub print_help
     $0 -h                               // print this message
     $0 -t template                      // generate case update template
     $0 -t find -c [case number]         // find the case update 
-\n"
+    $0 -t ir -l cn                      // generate IR message
+
+    Suggested alias:
+
+    alias sf='perl $0 -t template'
+    alias case_finder='perl $0 -t find -c'
+    alias ir='perl $0 -t ir -l'
+    alias ircn='perl $0 -t ir -l cn'
+\n";
+    exit 1;
+}
+
+sub generate_ir
+{
+    my $lang = shift;
+    my $output = "";
+
+    if ($lang =~ /cn/i)   ## cn version
+    {
+        $output = 'Hi,
+
+您好, 感谢联系Pivotal Support, 我是工程师Matt, 很高兴为您服务.
+我会检查case的记录并稍后联系您.
+
+Best Regards,
+
+Matt Song
+APJ Data Customer Engineer | Pivotal Support
+Email:   xsong@pivotal.io
+Working Hours: Mon-Fri 8 AM to 5 PM GMT+8';
+    }
+    else                    ## en version
+    {
+        $output = 'Hi,
+
+This is Matt from Pivotal support and I will assist you with this.
+
+Let me review the requested information and I will get back to you shortly
+
+Best Regards,
+
+Matt Song
+APJ Data Customer Engineer | Pivotal Support
+Email:   xsong@pivotal.io
+Working Hours: Mon-Fri 8 AM to 5 PM GMT+8';
+    }
+    print "$output"."\n";
+    return 0;
 }
 
 sub find_case_update
 {
     my $case = shift;
+
+    if (! $case)
+    {
+        print "No case number was specified, exit!\n";
+        &print_help;
+    }
+
     my $case_update_file = "${work_folder}/${case}/case_update_${case}.txt";
 
     if (-f $case_update_file)
@@ -109,7 +160,7 @@ sub generate_template
     my $case_update = "
 ================ GENERAL INFO ================
 
-Case Number:    $case_no
+Case#:          $case_no
 Product:        $product
 Version:        $version
 Severity:       $cur_sev
