2bfb6c8cf80dfd50baef2b0144da7c5aa9897ab7
Matt Song <matt.song@live.cn>
2019-05-02 14:41:22 +0800

    Updated at 2019-05-02

--- scripts/sync_git.sh
+++ scripts/sync_git.sh
@@ -1,4 +1,4 @@
 echo "Sync to aio2"
-rsync -av --exclude=".git*" ~/Pivotal_Work/ gpadmin@aio2:~/git_repository/
+rsync -av --delete --exclude=".git*" ~/Pivotal_Work/ gpadmin@aio2:~/git_repository/
 echo "sync to mdw"
-rsync -av --exclude=".git*" ~/Pivotal_Work/ gpadmin@mdw:~/git_repository/
+rsync -av --delete --exclude=".git*" ~/Pivotal_Work/ gpadmin@mdw:~/git_repository/
