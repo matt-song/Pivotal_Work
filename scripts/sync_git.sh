7e746b2d2863c9abdfdedefc48ad9588282bf9e2
Matt Song <matt.song@live.cn>
2019-05-06 10:53:56 +0800

    Updated at 2019-05-06

--- scripts/sync_git.sh
+++ scripts/sync_git.sh
@@ -1,4 +1,27 @@
-echo "Sync to aio2"
-rsync -av --delete --exclude=".git*" ~/Pivotal_Work/ gpadmin@aio2:~/git_repository/
-echo "sync to mdw"
-rsync -av --delete --exclude=".git*" ~/Pivotal_Work/ gpadmin@mdw:~/git_repository/
+#!/bin/bash
+# Note: the BASH on MAC does not support hash in declare... shit
+
+source_dir='/Users/xsong/Pivotal_Work/'
+
+conf='
+aio2|~/git_repository/
+mdw|~/git_repository/
+smdw|/data/matt/Pivotal_Work/
+'
+
+for line in `echo $conf | grep -v "^$"`
+do
+    host=`echo $line | awk -F'|' '{print $1}'`
+    target_dir=`echo $line | awk -F'|' '{print $2}'`
+    
+    echo "Sync data from [$source_dir] to host [$host], target folder [$target_dir]..."
+    
+    ### make sure the host is alive
+    ping_result=`ping -c1 -W1  $host 2>/dev/null | grep "packets received" 2>/dev/null | awk '{print $4}'`
+    if [ "x$ping_result" == 'x1' ]
+    then
+        rsync -av --delete --exclude=".git*" $source_dir gpadmin@${host}:$target_dir
+    else
+        echo "[ERROR] Host [$host] is down, skip..."
+    fi
+done
