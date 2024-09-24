a96dab94a0b8ef9665d130ee6bf1add41b34b0bf
matt-song <matt.song@live.cn>
2024-09-24 15:27:41 +0800

    Updated at 2024-09-24

---
+++ for_cloud_vm/monitor.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+: ' 
+Note: this script will monitor the performance and abnormal logs, send the report to target user.
+Crontab: 
+'
+mailList='xxxx@xxx.xx'
+tmpReportFile="/tmp/.monitor_report.log"
+needSendMail=0
+
+echo -e "
+========================================================
+System health check report
+generated on:\t `date`
+hostname:\t `hostname`
+========================================================
+" | tee $tmpReportFile
+
+check_load()
+{
+    echo "=== Checking the CPU load for last mintue ===" | tee -a $tmpReportFile
+    cpuCount=`cat /proc/cpuinfo | grep processor | wc -l`
+    loadLast1M=`uptime | awk -F'load average:' '{print $2}' | sed 's/,//g' | awk '{print $1}'`
+    loadLast1mInt=`echo $loadLast1M | sed 's/\..*//g'`
+    if [ $loadLast1mInt -ge $cpuCount ]
+    then
+        needSendMail=1
+        echo "The load [$loadLast1M] is pretty high, output of the # uptime: " | tee -a $tmpReportFile
+        uptime | tee -a $tmpReportFile
+    else
+        echo "Load is good, last 1 min's load is: [$loadLast1M]" | tee -a $tmpReportFile
+    fi 
+
+    echo -e "\n=== Checking top 10 CPU consumer: ===" | tee -a $tmpReportFile
+    top -b -n 1 | grep "^ " | sort -k 9 -nr | grep -v "^    PID" | head -10 | tee -a $tmpReportFile 
+}
+
+audit_log()
+{
+    today=`date | awk '{print $2,$3}'`
+    echo -e "\n=== Checking security logs of [$today] ===" | tee -a $tmpReportFile 
+    echo -e "\n=> session opened for user list: "  | tee -a $tmpReportFile 
+    sudo cat /var/log/secure  | grep "$today" | grep sshd | grep "session opened for user" | awk '{print $11}' | sort | uniq -c | sort -nr  | tee -a $tmpReportFile 
+    echo -e "\n=> Connection closed by authenticating user list: "  | tee -a $tmpReportFile  
+    sudo cat /var/log/secure  | grep "$today" | grep sshd  | grep 'Connection closed by authenticating user' | awk '{print "user: "$11,"\thost:"$12}' | sort | uniq -c | sort -nr  | tee -a $tmpReportFile 
+}
+
+check_network()
+{
+    echo -e "\n=== Checking network connections" | tee -a $tmpReportFile 
+    sudo netstat -antp 2>/dev/null | grep "^tcp" | awk '{print $6}' | sort | uniq -c | sort -nr | tee -a $tmpReportFile 
+}
+
+
+check_load
+audit_log
+check_network
+echo "" | tee -a $tmpReportFile 
+
+if [ x"$needSendMail" = 'x1' ]
+then
+    echo "Sending mail to [$mailList]..."
+    cat $tmpReportFile | mail -s "Health check report for [`hostname`] at [`date`]" $mailList
+fi
+
---
+++ scripts/enable_proxy.sh
@@ -0,0 +1,2 @@
+export http_proxy="socks5://localhost:1086"
+export https_proxy="socks5://localhost:1086"
